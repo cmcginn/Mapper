e154ea232c93734ba27db3cca29f5890f1fdad11
Chris McGinn <chris.s.mcginn@live.com>
2015-03-07 14:06:24 -0500

    Initial Project Creation

---
+++ Mapper.Web/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Mapper.Web.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Mapper.Web/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Mapper.Web.WebApiApplication" Language="C#" %>
